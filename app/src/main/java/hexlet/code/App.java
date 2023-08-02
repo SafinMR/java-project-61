ace417e344a0f62a84d1b3e8ec255224e830afa2
Marat Safin <safinmr229@gmail.com>
2023-08-03 02:06:48 +0300

    make game selection menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,18 @@
 package hexlet.code;
 
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
-        Cli.setName();
+    public static void main(String[] args) throws IOException {
+        char choice;
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet\n" + "0 - Exit");
+        choice = (char) System.in.read();
+        System.out.println("Your choice: " + choice);
+
+        if (choice == '1') {
+            Cli.setName();
+        }
     }
 }
