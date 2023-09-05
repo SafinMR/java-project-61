ade89f7da6b0b9847f3e876abf5787cb45784504
Marat Safin <safinmr229@gmail.com>
2023-09-05 23:55:57 +0300

    add Progression.java, update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -9,7 +9,7 @@ public class App {
         char choice;
 
         System.out.println("Please enter the game number and press Enter.");
-        System.out.println("1 - Greet\n" + "2 - Even\n" + "3 - Calc\n" + "4 - GCD\n" + "0 - Exit");
+        System.out.println("1 - Greet\n" + "2 - Even\n" + "3 - Calc\n" + "4 - GCD\n" + "5 - Progression\n" + "0 - Exit");
         choice = (char) System.in.read();
         System.out.println("Your choice: " + choice + "\n");
 
@@ -27,6 +27,10 @@ public class App {
             case '4' -> {
                 Engine.greeting();
                 GCD.playGCD();
+            }
+            case '5' -> {
+                Engine.greeting();
+                Progression.playProgression();
             }
             default -> {
 
---
+++ app/src/main/java/hexlet/code/games/code/Progression.java
@@ -0,0 +1,44 @@
+package hexlet.code.games.code;
+
+import hexlet.code.Engine;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Progression {
+    public static void playProgression() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What number is missing in the progression?");
+        int correctAnswerCount = 0;
+        while (correctAnswerCount < 3) {
+            int length = new Random().nextInt(5) + 5;
+            String[] progression = new String[length];
+            int firstNum = new Random().nextInt(100);
+            int diff = new Random().nextInt(20);
+
+            for (int i = 0; i < length; i ++) {
+                progression[i] = String.valueOf(firstNum + i * diff);
+            }
+
+            int misNum = new Random().nextInt(length - 1);
+            String correctAnswer = progression[misNum];
+            progression[misNum] = "..";
+
+            System.out.print("Question: ");
+            for (int i = 0; i < length; i++) {
+                System.out.print(progression[i] + " ");
+            }
+            System.out.println();
+            System.out.print("Answer:  ");
+            String answer = scanner.next();
+
+            if (Engine.correctAnswerCheck(answer, correctAnswer)) {
+                correctAnswerCount++;
+            } else {
+                return;
+            }
+        }
+        System.out.println("Congratulations, " + Engine.userName + "!");
+        scanner.close();
+    }
+}
