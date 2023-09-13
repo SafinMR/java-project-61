32fb8b440c018f65f1e0f27ef6740ca47135ec2c
Marat Safin <safinmr229@gmail.com>
2023-09-14 01:06:29 +0300

    refactoring

--- app/src/main/java/hexlet/code/games/code/Progression.java
+++ app/src/main/java/hexlet/code/games/code/Progression.java
@@ -6,7 +6,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Progression {
-    static String[] getArray(int length) {
+    static String getArray() {
+        final int min = 5;
+        final int max = 5;
+        int length = new Random().nextInt(min) + max;
+
         final int bound1 = 100;
         final int bound2 = 20;
         int firstNum = new Random().nextInt(bound1);
@@ -16,7 +20,18 @@ public class Progression {
         for (int i = 0; i < length; i++) {
             progression[i] = String.valueOf(firstNum + i * diff);
         }
-        return progression;
+        int bound = length - 1;
+        int missNum = new Random().nextInt(bound);
+        String correctAnswer = progression[missNum];
+        progression[missNum] = "..";
+
+        System.out.print("Question: ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(progression[i] + " ");
+        }
+        System.out.println();
+
+        return correctAnswer;
     }
 
     public static void playProgression() {
@@ -25,21 +40,8 @@ public class Progression {
         System.out.println("What number is missing in the progression?");
         int correctAnswerCount = 0;
         while (correctAnswerCount < 3) {
-            final int min = 5;
-            final int max = 5;
-            int length = new Random().nextInt(min) + max;
-            String[] progression = getArray(length);
+            String correctAnswer = getArray();
 
-            int bound = length - 1;
-            int misNum = new Random().nextInt(bound);
-            String correctAnswer = progression[misNum];
-            progression[misNum] = "..";
-
-            System.out.print("Question: ");
-            for (int i = 0; i < length; i++) {
-                System.out.print(progression[i] + " ");
-            }
-            System.out.println();
             System.out.print("Your answer: ");
             String answer = scanner.next();
 
