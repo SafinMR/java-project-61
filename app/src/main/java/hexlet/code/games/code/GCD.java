2c4b7fdd884f98b518392e853d7dfaa7a0f11c68
Marat Safin <safinmr229@gmail.com>
2023-09-17 14:25:56 +0300

    refactoring GCD

--- app/src/main/java/hexlet/code/games/code/GCD.java
+++ app/src/main/java/hexlet/code/games/code/GCD.java
@@ -24,10 +24,9 @@ public class GCD {
         for (int i = 1; i <= a; i++) {
             boolean n1 = randomNum1 % i == 0;
             boolean n2 = randomNum2 % i == 0;
-            if (n1) {
-                if (n2) {
-                    answer = i;
-                }
+            boolean n = n1 & n2;
+            if (n) {
+                answer = i;
             }
         }
 
